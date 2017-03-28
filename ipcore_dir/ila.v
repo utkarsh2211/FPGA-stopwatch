b87836e1cffeaa9e7158a610236990157f4e2682
Utkarsh Verma <utkarsh874@gmail.com>
2017-03-29 01:42:16 +0530

    Add ila and icon cores

---
+++ ipcore_dir/ila.v
@@ -0,0 +1,33 @@
+///////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2017 Xilinx, Inc.
+// All Rights Reserved
+///////////////////////////////////////////////////////////////////////////////
+//   ____  ____
+//  /   /\/   /
+// /___/  \  /    Vendor     : Xilinx
+// \   \   \/     Version    : 14.7
+//  \   \         Application: Xilinx CORE Generator
+//  /   /         Filename   : ila.v
+// /___/   /\     Timestamp  : Wed Mar 29 01:19:31 India Standard Time 2017
+// \   \  /  \
+//  \___\/\___\
+//
+// Design Name: Verilog Synthesis Wrapper
+///////////////////////////////////////////////////////////////////////////////
+// This wrapper is used to integrate with Project Navigator and PlanAhead
+
+`timescale 1ns/1ps
+
+module ila(
+    CONTROL,
+    CLK,
+    DATA,
+    TRIG0) /* synthesis syn_black_box syn_noprune=1 */;
+
+
+inout [35 : 0] CONTROL;
+input CLK;
+input [31 : 0] DATA;
+input [7 : 0] TRIG0;
+
+endmodule
